08f47e3b5264f8c23baf05dc4a387fea6238fc98
泓熙 辜 <justlove0714@hotmail.com>
2016-02-04 13:06:03 +0800

    google authentication demo added.

--- www/js/app.js
+++ www/js/app.js
@@ -3,89 +3,62 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services','firebase'])
+angular.module('starter', ['ionic','firebase'])
 
-
-.constant( "settings", { "stage": true, "company": "asshole" })
-.constant( "devFB", "https://pos-test-pku.firebaseio.com/")
-.constant( "prodFB", "https://pos-test-pku.firebaseio.com/")
+.factory("Auth", function($firebaseAuth) {
+  var usersRef = new Firebase("https://pos-test-pku.firebaseio.com/users/");
+  return $firebaseAuth(usersRef)
+})
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      // Don't remove this line unless you know what you are doing. It stops the viewport
+      // from snapping when text inputs are focused. Ionic handles this internally for
+      // a much nicer keyboard experience.
       cordova.plugins.Keyboard.disableScroll(true);
-
     }
-
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
+    if(window.StatusBar) {
       StatusBar.styleDefault();
-    } 
-
+    }
   });
 })
-.config(function($stateProvider, $urlRouterProvider) {
+.controller('LoginCtrl',function($scope,$ionicModal,Auth){
+  $scope.user = {};
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
-
-  // setup an abstract state for the tabs directive
-    .state('tab', {
-    url: '/tab',
-    abstract: true,
-    templateUrl: 'templates/tabs.html'
-  })
-
-  // Each tab has its own nav history stack:
-
-  .state('tab.dash', {
-    url: '/dash',
-    views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
-      }
-    }
-  })
-
-  .state('tab.chats', {
-      url: '/chats',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
-        }
-      }
-    })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
-        }
-      }
-    })
-
-  .state('tab.account', {
-    url: '/account',
-    views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
-      }
+  $ionicModal.fromTemplateUrl('login.html', {
+    scope: $scope,
+    animation: 'slide-in-up',
+  }).then(function(modal) {
+    $scope.modal = modal;
+    var authData = Auth.$getAuth();
+    if( authData ) {
+      $scope.user = authData;
+    } else {
+      $scope.modal.show();
     }
   });
 
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
-
-});
+  $scope.UserLogin = function(type) {
+    Auth.$authWithOAuthRedirect(type).then(function(authData) {
+      // User successfully logged in
+      $scope.user = authData;
+    }).catch(function(error) {
+      if (error.code === "TRANSPORT_UNAVAILABLE") {
+        Auth.$authWithOAuthPopup(type).then(function(authData) {
+          // User successfully logged in. We can log to the console
+          // since we’re using a popup here
+          $scope.user = authData;
+          $scope.modal.hide();
+          console.log(authData);
+        });
+      } else {
+        // Another error occurred
+        console.log(error);
+      }
+    });
+  }
+})
